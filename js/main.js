ce77074af9f16a1c68cc37ffad14672907153515
sabrina <sabrina.bosco89@gmail.com>
2016-12-27 16:21:18 +0100

    add switch animal function log

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var GameState = {
     preload: function() {
         this.load.image('background', 'assets/images/background.jpg');
         this.load.image('chicken', 'assets/images/chicken.png');
+        this.load.image('horse', 'assets/images/horse.png');
+        this.load.image('pig', 'assets/images/pig.png');
+        this.load.image('arrow', 'assets/images/arrow.png');
     },
     create: function() {
         
@@ -16,9 +19,34 @@ var GameState = {
         this.chicken.anchor.setTo(0.5, 0.5);
         this.chicken.scale.setTo(0.5);
         this.chicken.angle = 90;
+        
+        // arrow right
+        this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+        this.rightArrow.anchor.setTo(0.5);
+        this.rightArrow.scale.setTo(0.2);
+        this.rightArrow.customParams = {direction: 1};
+        
+        // arrow left
+        this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+        this.leftArrow.anchor.setTo(0.5);
+        this.leftArrow.scale.setTo(0.2);
+        this.leftArrow.customParams = {direction: -1};
+        
+        // input enabled left
+        this.leftArrow.inputEnabled = true;
+        this.leftArrow.input.pixelPerfectClick = true;
+        this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+        
+        // input enabled right
+        this.rightArrow.inputEnabled = true;
+        this.rightArrow.input.pixelPerfectClick = true;
+        this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
     },
     update: function() {
         this.chicken.angle += 0.5;
+    },
+    switchAnimal: function(sprite, event) {
+        console.log('move animal');
     }
 };
 
