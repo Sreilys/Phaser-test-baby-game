7612119d7fbc2cac5c4cd0d88544f6ff90f98edc
sabrina <sabrina.bosco89@gmail.com>
2016-12-27 09:40:27 +0100

    scale the game view for different size of devices

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ var GameState = {
         this.load.image('chicken', 'assets/images/chicken.png');
     },
     create: function() {
+        
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        
         this.background = this.game.add.sprite(0, 0, 'background');
         this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
         this.chicken.anchor.setTo(0.5, 0.5);
