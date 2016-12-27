05c5abff72ac42a21b602619dc6e49acf27e4184
sabrina <sabrina.bosco89@gmail.com>
2016-12-27 09:34:29 +0100

    rotate target image

--- js/main.js
+++ js/main.js
@@ -9,9 +9,11 @@ var GameState = {
         this.background = this.game.add.sprite(0, 0, 'background');
         this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
         this.chicken.anchor.setTo(0.5, 0.5);
+        this.chicken.scale.setTo(0.5);
+        this.chicken.angle = 90;
     },
     update: function() {
-        
+        this.chicken.angle += 0.5;
     }
 };
 
